bea6265cf9780706768af8abe74d2505cf4d9a75
SSujjall <suzallmhz@gmail.com>
2024-12-31 11:04:20 +0545

    Cloudinary Init

---
+++ BlogApp.Application/Helpers/CloudinaryService/CloudinaryService.cs
@@ -0,0 +1,83 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace BlogApp.Application.Helpers.CloudinaryService
+{
+    public class CloudinaryService : ICloudinaryService
+    {
+        private readonly Cloudinary _cloudinary;
+        private readonly ILogger<CloudinaryService> _logger;
+        private readonly string folderName = "BlogAppPhotos";
+
+        public CloudinaryService(Cloudinary cloudinary, ILogger<CloudinaryService> logger)
+        {
+            _cloudinary = cloudinary;
+            _logger = logger;
+        }
+
+        public bool GetFolders()
+        {
+            var res = _cloudinary.SearchFolders().Expression($"name={folderName}").Execute();
+            var resultJson = JsonConvert.SerializeObject(res);
+            JToken token = JToken.Parse(resultJson);
+
+            bool folderExists = token["folders"]?.Any(x => x["path"]?.ToString() == folderName) ?? false;
+
+            if (folderExists) return true;
+
+            else return false;
+        }
+
+        public async Task<string> UploadImage(IFormFile file)
+        {
+            if (file.Length > 0)
+            {
+                var folderExists = GetFolders();
+                if (!folderExists)
+                {
+                    // create folder if it does not exists.
+                    _cloudinary.CreateFolder(folderName);
+                }
+
+                using var stream = file.OpenReadStream();
+                var uploadParam = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Folder = folderName,
+                };
+
+                var uploadRes = await _cloudinary.UploadAsync(uploadParam);
+                if (uploadRes.StatusCode == HttpStatusCode.OK)
+                {
+                    return uploadRes.SecureUrl.ToString();
+                }
+            }
+            return null;
+        }
+
+        public Task<string> UploadMultipleImage(List<IFormFile> files)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteImage(string publicId)
+        {
+            if (string.IsNullOrEmpty(publicId))
+            {
+                return;
+            }
+            var deleteResult = await _cloudinary.DeleteResourcesAsync(ResourceType.Image, publicId);
+
+            if (deleteResult.StatusCode != HttpStatusCode.OK)
+            {
+                _logger.LogError("Failed to delete image with PublicId: {PublicId}. StatusCode: {StatusCode}",
+                       publicId, deleteResult.StatusCode);
+            }
+        }
+    }
+}
---
+++ BlogApp.Application/Helpers/CloudinaryService/ICloudinaryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+
+namespace BlogApp.Application.Helpers.CloudinaryService
+{
+    public interface ICloudinaryService
+    {
+        Task<string> UploadImage(IFormFile file);
+        Task<string> UploadMultipleImage(List<IFormFile> files);
+        Task DeleteImage(string publicId);
+    }
+}
