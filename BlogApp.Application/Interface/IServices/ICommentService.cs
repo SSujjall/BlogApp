90f8a5d5fbae47e9928b641281dd67672ba50c42
Sujal Maharjan <sum0590@my.londonmet.ac.uk>
2025-01-04 18:27:42 +0545

    Comment CRUD started

---
+++ BlogApp.Application/Interface/IServices/ICommentService.cs
@@ -0,0 +1,16 @@
+﻿using BlogApp.Application.DTOs;
+using BlogApp.Application.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Interface.IServices
+{
+    public interface ICommentService
+    {
+        Task<ApiResponse<IEnumerable<CommentDTO>>> GetAllComment(int blogId);
+        Task<ApiResponse<CommentDTO>> AddComment(AddCommentDTO dto);
+    }
+}
---
+++ BlogApp.Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlogApp.Application.Interface.IRepositories;
+using BlogApp.Domain.Entities;
+using BlogApp.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Infrastructure.Repositories
+{
+    public class CommentRepository : BaseRepository<Comments>, ICommentRepository
+    {
+        public CommentRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
