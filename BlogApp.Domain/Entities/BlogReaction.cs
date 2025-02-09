541c5f02accc6dbb4e00c259a848b39f8e4fe543
SSujjall <suzallmhz@gmail.com>
2025-02-09 17:19:17 +0545

    Removed reaction feature for reset

--- BlogApp.Domain/Entities/BlogReaction.cs
+++ BlogApp.Domain/Entities/BlogReaction.cs
@@ -1,16 +1,25 @@
-﻿using BlogApp.Domain.Entities.Abstracts;
-using BlogApp.Domain.Shared;
+﻿using BlogApp.Domain.Shared;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlogApp.Domain.Entities
 {
-    public class BlogReaction : Reaction
+    public class BlogReaction
     {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = null!;
+
         [ForeignKey(nameof(Blogs))]
         public int BlogId { get; set; }
 
+        public VoteType ReactionType { get; set; }
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
         // Navigation Property
         public virtual Blogs Blogs { get; set; } = null!;
+        public virtual Users User { get; set; } = null!;
     }
 }
--- BlogApp.Domain/Entities/CommentReaction.cs
+++ BlogApp.Domain/Entities/CommentReaction.cs
@@ -1,16 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using BlogApp.Domain.Shared;
-using BlogApp.Domain.Entities.Abstracts;
 
 namespace BlogApp.Domain.Entities
 {
-    public class CommentReaction : Reaction
+    public class CommentReaction
     {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = null!;
+
         [ForeignKey(nameof(Comments))]
         public int CommentId { get; set; }
 
+        public VoteType ReactionType { get; set; }
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
         // Navigation Property
         public virtual Comments Comments { get; set; } = null!;
+        public virtual Users User { get; set; } = null!;
     }
 }
