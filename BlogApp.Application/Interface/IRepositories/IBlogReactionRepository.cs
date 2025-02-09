8a32a5137f04c7ff6c78374be4a1d33b5e93e097
SSujjall <suzallmhz@gmail.com>
2025-02-09 17:29:16 +0545

    Blog Reaction initial

---
+++ BlogApp.Application/Interface/IRepositories/IBlogReactionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogApp.Domain.Entities;
+
+namespace BlogApp.Application.Interface.IRepositories
+{
+    public interface IBlogReactionRepository : IBaseRepository<BlogReaction>
+    {
+    }
+}
---
+++ BlogApp.Infrastructure/Services/BlogReactionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogApp.Application.Interface.IServices;
+
+namespace BlogApp.Infrastructure.Services
+{
+    public class BlogReactionService : IBlogReactionService
+    {
+    }
+}
