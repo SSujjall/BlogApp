452b402ce88183c9bccf61a107b6209941842cb8
SSujjall <suzallmhz@gmail.com>
2025-02-12 13:38:22 +0545

    Layout progres

--- BlogApp.ReactFrontend/src/components/layout/Searchbar.jsx
+++ BlogApp.ReactFrontend/src/components/layout/Searchbar.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from "react";
+import Button from "../common/Button";
 
-const SearchBar = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSearch = () => {
-    if (onSearch) {
-      onSearch(searchTerm);  // Call the search function passed as a prop
-    }
-  };
-
+const Searchbar = () => {
   return (
-    <div className="mb-4 flex items-center space-x-2">
-      <input
-        type="text"
-        className="border p-2 w-1/3"
-        placeholder="Search blogs..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-        onClick={handleSearch}
-      >
-        Search
-      </button>
-    </div>
+    <>
+      <nav className="fixed bg-red-400 p-3 w-full flex items-center justify-between">
+        <div className="pr-5 text-2xl font-bold">MyBlog</div>
+
+        <div className="flex-1 flex justify-center py-xs">
+          <div className="w-full max-w-[560px] m:mx-auto">
+            {/* Search bar */}
+            <input
+              type="text"
+              placeholder="Search"
+              className="w-full p-2 rounded-md"
+            />
+          </div>
+        </div>
+
+        <div className="pl-5 gap-xs flex items-center justify-end">
+          <Button text="Login" icon={"person"} iconSize={20} />
+        </div>
+      </nav>
+    </>
   );
 };
 
-export default SearchBar;
+export default Searchbar;
