ac9eb392ef46c2f56fae1049f8adafd2484d254a
SSujjall <suzallmhz@gmail.com>
2025-03-04 17:17:11 +0545

    BE: Change response from BadRequest to Accepted in AuthController register endpoint. FE: Implemented basic signup

--- BlogApp.ReactFrontend/src/features/auth/service/signupService.js
+++ BlogApp.ReactFrontend/src/features/auth/service/signupService.js
@@ -2,13 +2,8 @@ import { makeRequest } from "../../../common/services/api";
 
 export const signup = async (data) => {
   try {
-    return await makeRequest(
-      "POST",
-      "/Auth/register",
-      data
-    );
-  } catch (error) {
-    console.error("Error signup:", error);
+    return await makeRequest("POST", "/Auth/register", data);
+  } catch {
     return null;
   }
 };
