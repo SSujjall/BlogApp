0b32d5f1593c1b78e7404c67daa76b40ed2b55bc
Sujal Maharjan <sum0590@my.londonmet.ac.uk>
2025-03-16 21:49:48 +0545

    CI/CD implementation with github actions.

---
+++ BlogApp.API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("check")]
+        public IActionResult Get()
+        {
+            return Ok("API is working.");
+        }
+    }
+}
---
+++ BlogApp.Infrastructure/Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogApp.Infrastructure.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            // Set up configuration to read the connection string from environment variables
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables() // This will allow reading from environment variables
+                .Build();
+
+            // Retrieve the connection string from environment variables or appsettings.json
+            var connectionString = configuration.GetConnectionString("BlogDB")
+                                   ?? Environment.GetEnvironmentVariable("ConnectionStrings__BlogDB"); // This is variable from yml file env
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string 'BlogDB' not found.");
+            }
+
+            optionsBuilder.UseSqlServer(connectionString); // Or use the appropriate provider
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
