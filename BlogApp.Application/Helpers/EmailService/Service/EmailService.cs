c041ba73a78268a2d88e496ea5eecbc873b16180
SSujjall <suzallmhz@gmail.com>
2025-03-27 11:59:37 +0545

    Blog email service update

--- BlogApp.Application/Helpers/EmailService/Service/EmailService.cs
+++ BlogApp.Application/Helpers/EmailService/Service/EmailService.cs
@@ -18,7 +18,7 @@ namespace BlogApp.Application.Helpers.EmailService.Service
         private MimeMessage CreateEmailMessage(EmailMessage message)
         {
             var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress("email,", _emailConfig.From));
+            emailMessage.From.Add(new MailboxAddress("HakuBlogApp", _emailConfig.From));
             emailMessage.To.AddRange(message.To);
             emailMessage.Subject = message.Subject;
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
