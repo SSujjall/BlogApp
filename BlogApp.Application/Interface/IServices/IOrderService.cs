abd2b8c34aa2a34ead17af792b26652ae4e62310
Sujal Maharjan <suzallmhz@gmail.com>
2026-03-01 22:42:29 +0545

    Orders Service INIT

--- BlogApp.Application/Interface/IServices/IOrderService.cs
+++ BlogApp.Application/Interface/IServices/IOrderService.cs
@@ -1,10 +1,15 @@
 ﻿using BlogApp.Application.DTOs;
 using BlogApp.Application.Helpers.HelperModels;
+using BlogApp.Domain.Entities;
 
 namespace BlogApp.Application.Interface.IServices
 {
     public interface IOrderService
     {
-        Task<ApiResponse<object>> CreateOrder(CreateOrderDTO dto);
+        Task<ApiResponse<Orders>> CreateOrder(CreateOrderDTO dto);
+        Task<ApiResponse<IEnumerable<Orders>>> GetAllOrders();
+         Task<ApiResponse<Orders>> GetOrderById(int id);
+         Task<ApiResponse<Orders>> UpdateOrder(UpdateOrderDTO dto);
+         Task<ApiResponse<string>> DeleteOrder(int id);
     }
 }
--- BlogApp.Infrastructure/Services/OrderService.cs
+++ BlogApp.Infrastructure/Services/OrderService.cs
@@ -1,12 +1,33 @@
 ﻿using BlogApp.Application.DTOs;
 using BlogApp.Application.Helpers.HelperModels;
 using BlogApp.Application.Interface.IServices;
+using BlogApp.Domain.Entities;
 
 namespace BlogApp.Infrastructure.Services
 {
     public class OrderService : IOrderService
     {
-        public Task<ApiResponse<object>> CreateOrder(CreateOrderDTO dto)
+        public Task<ApiResponse<Orders>> CreateOrder(CreateOrderDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<string>> DeleteOrder(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<IEnumerable<Orders>>> GetAllOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<Orders>> GetOrderById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<Orders>> UpdateOrder(UpdateOrderDTO dto)
         {
             throw new NotImplementedException();
         }
